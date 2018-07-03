1a9ea6efcf81828c8899815e0d96246e76303057
Chong Vang <vangcguy@gmail.com>
2018-07-03 15:26:54 -0500

    added more to controller

---
+++ Superhero/Controllers/SuperheroController.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Superhero.Models;
+
+namespace Superhero.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Superhero 
+        public ActionResult Index()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var heroes = context.Superheroes.ToList();
+            return View(heroes);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Create([Bind(Include = "Id,Name,AlterEgo,PrimarySuperheroAbility,SecondarySuperheroAbility,Catchphrase")] Superheroes superhero)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Superheroes.Add(superhero);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(superhero);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Superheroes hero = db.Superheroes.Find(id);
+            if (hero == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hero);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Superheroes hero)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hero).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hero);
+        }
+
+        public ActionResult Details()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            return View();
+        }
+    }
+}
---
+++ Superhero/Models/Superheroes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Superhero.Models
+{
+    public class Superheroes
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string AlterEgo { get; set; }
+
+        public string PrimarySuperheroAbility { get; set; }
+
+        public string SecondarySuperheroAbility { get; set; }
+
+        public string Catchphrase { get; set; }
+
+    }
+}
